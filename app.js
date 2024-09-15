51d4211c09b87a515dc6eed3bd60dfcc26f95cbc
jayaramskumar <kumarjayaram545@gmail.com>
2024-09-15 18:58:44 +0530

    fixed port

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
+const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("listening on port 3000")
 })
 const { GoogleGenerativeAI } = require("@google/generative-ai");
